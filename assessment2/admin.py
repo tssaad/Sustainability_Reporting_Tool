a1b3f7579759573bfe324afb0e2b3004998c825e
tssaad <tssaad.g@gmail.com>
2021-12-13 22:26:16 +0100

    remove unused app

--- assessment2/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import WebAssessmentInfo, WebsiteAssessmentDetail
-
-admin.site.register(WebAssessmentInfo)
-admin.site.register(WebsiteAssessmentDetail)
